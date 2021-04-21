12dfa9946d603d312aabf3422244eff4cfda0cf0
David Schinazi <david.schinazi@gmail.com>
2021-04-20 22:25:13 -0700

    refactor code around

--- src/DFSparks.h
+++ src/DFSparks.h
@@ -2,9 +2,11 @@
 #define DFSPARKS_H
 
 #include "dfsparks/board.h"
+#include "dfsparks/button.h"
 #include "dfsparks/color.h"
 #include "dfsparks/player.h"
 #include "dfsparks/log.h"
 #include "dfsparks/networks/esp8266wifi.h"
+#include "dfsparks/vest.h"
 
 #endif /* DFSPARKS_H */
