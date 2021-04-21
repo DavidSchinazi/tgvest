1d108aca4f110138dc41f48202ce3433881f625e
David Schinazi <david.schinazi@gmail.com>
2021-04-20 22:28:14 -0700

    fix

--- src/DFSparks.h
+++ src/DFSparks.h
@@ -1,12 +1,15 @@
 #ifndef DFSPARKS_H
 #define DFSPARKS_H
 
+// This needs to be included first to ensure that the right macros
+// are defined before FastLED.h is included.
+#include "dfsparks/vest.h"
+
 #include "dfsparks/board.h"
 #include "dfsparks/button.h"
 #include "dfsparks/color.h"
 #include "dfsparks/player.h"
 #include "dfsparks/log.h"
 #include "dfsparks/networks/esp8266wifi.h"
-#include "dfsparks/vest.h"
 
 #endif /* DFSPARKS_H */
